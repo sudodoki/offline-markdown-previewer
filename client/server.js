4dd4c34dee755109e85585bc8cc85567bb0eb57c
zonzujiro <ititarenko@live.com>
2017-03-04 13:49:08 +0200

    Replacing indentations

--- client/server.js
+++ client/server.js
@@ -3,13 +3,13 @@ const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    contentBase: 'public/'
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  contentBase: 'public/'
 }).listen(8000, 'localhost', function (error) {
-    if (error) {
-        return console.log(error)
-    }
-    console.log('Server running at http://localhost:8000/')
+  if (error) {
+    return console.log(error)
+  }
+  console.log('Server running at http://localhost:8000/')
 })
