d1043beabb42c3e2a622ad259e362eb8d133bfe0
Oleh Aloshkin <olegaleshkin@gmail.com>
2017-03-04 14:05:38 +0200

    Fix .eslint for webstorm

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const serveStatic = require('serve-static')
 
-const app = express()
+const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client')))
 app.listen(process.env.npm_package_config_port)
