422ca77f08c620b9833c7261baede0c6a7ceffc1
zonzujiro <ititarenko@live.com>
2017-03-04 18:07:38 +0200

    Removing server todo, fixing babelrc, add port config

--- client/server.js
+++ client/server.js
@@ -7,9 +7,9 @@ new WebpackDevServer(webpack(config), {
   hot: true,
   historyApiFallback: true,
   contentBase: 'public/'
-}).listen(8000, 'localhost', function (error) {
+}).listen(process.env.npm_package_config_port, 'localhost', function (error) {
   if (error) {
     return console.log(error);
   }
-  console.log('Server running at http://localhost:8000/');
+  console.log(`Server running at http://localhost:${process.env.npm_package_config_port}/`);
 });
